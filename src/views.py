7b0d284d36f1ef293ba7b86b6f80dfdf32f03a0c
Lauren Blakley <CowgirlXena@gmail.com>
2020-02-24 09:52:38 -0800

    got up to the point where a simple new player object can be created. Not working how we want completely just yet

--- src/views.py
+++ src/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from players.models import Player
+from rest_framework import request
 from rest_framework.response import Response
 
 from src.api.serializers import PlayerSerializer
@@ -12,21 +13,29 @@ class HomePageView(TemplateView):
     template_name = 'home_page.html'
 
 
-def new_player(request):
-    add_player = Player.objects.create(name=request.user.player, user=request.user.username)
-    add_player.user.player = add_player
-    # add_player.user.player.save()
-    add_player.save()
-    serializer = PlayerSerializer(add_player, many=True, context={'request': request})
-    return serializer.data
-
-
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-    def post(self, request, *args, **kwargs):
-        return super().post(self)
+    player = Player.objects.all()
+    add_player = Player.objects.create(name=request.player, user=request.user.username)
+    add_player.save()
 
 
+
+#     def post(self, request, *args, **kwargs):
+#         return super().post(self)
+#
+#
+# def new_player(request):
+#     player = Player.objects.all()
+#     add_player = player(name="TestNumber2")
+#     add_player.save()
+#
+#     add_player = Player.objects.create(name=request.user.player, user=request.user.username)
+#     add_player.user.player = add_player
+#     add_player.user.player.save()
+#     add_player.save()
+#     serializer = PlayerSerializer(add_player, many=True, context={'request': request})
+#     return serializer.data
