8c6a41bbe8a2aef4cfb91757910924521f39e5ce
Lauren Blakley <CowgirlXena@gmail.com>
2020-06-20 08:38:41 -0700

    made changes to wsgi file

--- src/wsgi.py
+++ src/wsgi.py
@@ -2,6 +2,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', "settings.base")
-
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "settings.base")
