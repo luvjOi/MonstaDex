0008c828a46013f97c5e762f6f80ef63c84b997e
Lauren Blakley <CowgirlXena@gmail.com>
2020-06-22 14:02:44 -0700

    changing it back to the right path

--- src/wsgi.py
+++ src/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', "test")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "src.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
